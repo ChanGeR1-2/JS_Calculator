facd0d86a8952f416f03bc5a7d10a5f00335b826
ChanGeR1-2 <andrewvine89@gmail.com>
2024-07-09 13:25:17 +0900

    Completed all features

--- app.js
+++ app.js
@@ -1,49 +1,109 @@
 const numberButtons = document.querySelector(".buttons");
-const display = document.querySelector(".display");
-let displayValue = '0';
+const bottomDisplay = document.querySelector("#bottom-display");
+const topDisplay = document.querySelector("#top-display span");
 
-let firstNumber = null;
-let secondNumber = null;
-let operator = null;
-let reset = false;
+let currentNumber = [];
+let nums = [];
+let ops = [];
+
+let resetValues = false;
+let decimalPointValueChange = false;
 
 numberButtons.addEventListener("click", (event) => {
-    if (event.target.classList.contains("number-button")) {
-        displayValue = displayValue === '0' || reset ? event.target.textContent : displayValue + event.target.textContent;
-        reset = false;
-    } else if (event.target.classList.contains("util-button")) {
-        switch(event.target.id) {
-            case "reset-button":
-                displayValue = '0';
-                firstNumber = null;
-                secondNumber = null;
-                operator = null;
-        }
-    } else if (event.target.classList.contains("sign-button")) {
-        if (operationPresent()) {
-            secondNumber = parseInt(displayValue);
-            const result = operate(operator, firstNumber, secondNumber)
-            displayValue = result.toString();
-            firstNumber = result;
-            secondNumber = null;
-            operator = event.target.textContent;
-        } else {
-            operator = event.target.textContent;
-            firstNumber = parseInt(displayValue);
-        }
-        reset = true;
-    } else if (event.target.id === "equals-button") {
-        secondNumber = parseInt(displayValue);
-        displayValue = operate(operator, firstNumber, secondNumber).toString();
-        firstNumber = null;
-        secondNumber = null;
-        operator = null;
+    const target = event.target;
+    if (resetValues) {
+        nums = [];
+        ops = [];
+        resetValues = false;
     }
-    display.textContent = displayValue;
+    if (target.classList.contains("number-button")) {
+        if (decimalPointValueChange) {
+            currentNumber[currentNumber.length - 1] = ".";
+            decimalPointValueChange = false;
+        }
+        currentNumber.push(target.textContent);
+    } else if (target.classList.contains("util-button")) {
+        switch (target.id) {
+            case "reset-button":
+                reset();
+                break;
+            case "change-sign-button":
+                if (currentNumber[0]) {
+                    if (currentNumber[0].charAt(0) === "-") {
+                        currentNumber[0] = currentNumber[0].slice(1);
+                    } else {
+                        currentNumber[0] = "-" + currentNumber[0];
+                    }
+                }
+                break;
+            case "back-button":
+                const num = currentNumber[currentNumber.length - 1]
+                currentNumber[currentNumber.length - 1] = num.slice(0, num.length - 2);
+                if (!currentNumber[currentNumber.length - 1]) {
+                    currentNumber.pop();
+                }
+                break;
+        }
+    } else if (target.classList.contains("sign-button")) {
+        pushNumber();
+        ops.push(target.textContent);
+        if (ops[1]) {
+            calculate();
+            nums = [];
+            ops.shift();
+            pushNumber();
+        }
+    } else if (target.id === "equals-button") {
+        pushNumber();
+        if (calculate()) {
+            resetValues = true;
+        }
+    } else if (target.id === "point-button") {
+        if (currentNumber.filter((str) => str.includes(".")).length === 0) {
+            currentNumber.push(currentNumber.length === 0 ? "0.0" : ".0");
+            decimalPointValueChange = true;
+        }
+    }
+    refresh();
 })
 
-function operationPresent() {
-    return firstNumber && operator;
+function refresh() {
+    bottomDisplay.textContent = currentNumber.length > 0 ? currentNumber.join('') : '0';
+    let displayString = '';
+    if (ops[0]) {
+        displayString += nums[0] + " " + ops[0];
+        if (nums[1]) {
+            displayString += " " + nums[1] + " =";
+        }
+    }
+    topDisplay.textContent = displayString;
+}
+
+function calculate() {
+    const result = operate(ops[0], nums[0], nums[1]);
+    if (result) {
+        currentNumber = [];
+        currentNumber.push(result.toString());
+        return true;
+    }
+    return false;
+}
+
+function pushNumber() {
+    if (currentNumber[0]) {
+        if(currentNumber.filter((str) => str.includes(".")).length === 0) {
+            nums.push(parseInt(currentNumber.join('')));
+        } else {
+            nums.push(parseFloat(currentNumber.join('')));
+        }
+        currentNumber = [];
+    }
+}
+
+function reset() {
+    currentNumber = [];
+    nums = [];
+    ops = [];
 }
 
 function add(a, b) {
@@ -67,7 +127,8 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    switch(operator) {
+    if (a === undefined || b === undefined || !operator) return;
+    switch (operator) {
         case '+':
             return add(a, b);
         case '-':
